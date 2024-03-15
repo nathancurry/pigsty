5131433bbd333bbe59b1578fe8a9359a2ef4c379
Nathan Curry <nathancurry@gmail.com>
2024-03-15 18:04:06 -0400

    add ckls and oscap xccdf

---
+++ src/pigsty/resources/checklist.py
@@ -0,0 +1,320 @@
+"""
+Module to provide dictionary-like interface to DISA checklist files produced by STIG Viewer 2.
+"""
+
+from pathlib import Path
+from typing import Dict, List
+from xml.etree import ElementTree as ET
+
+
+class DictNode():
+    """
+    Interface for dictionary node.
+    """
+
+    def __init__(self, node: ET.Element, prefix: str, suffix: str, immutable: bool):
+        self.node: ET.Element = node
+        self.__prefix: str = prefix
+        self.__suffix: str = suffix
+        self.immutable: bool = immutable
+
+    def get(self, key: str) -> str:
+        """
+        Get value for key.
+
+        Args:
+            key (str): Key
+
+        Returns:
+            str: Value
+
+        Raises:
+            KeyError: If key not found
+        """
+        try:
+            return self.node.find(f"{self.__prefix}{key}{self.__suffix}").text
+        except AttributeError as exc:
+            raise KeyError(f"{key} not found in {self.node.tag}") from exc
+
+    def set(self, key: str, value: str):
+        """
+        Set value for key.
+
+        Args:
+            key (str): Key
+            value (str): Value
+
+        Raises:
+            PermissionError: If node is write protected
+            KeyError: If key not found
+        """
+        if self.immutable:
+            raise PermissionError(f"Node is write protected. Cannot set {key} to {value}")
+        try:
+            self.node.find(f"{self.__prefix}{key}{self.__suffix}").text = value
+        except AttributeError as exc:
+            raise KeyError(f"{key} not found in {self.node.tag}") from exc
+
+    def items(self):
+        """
+        Returns list of (key, value) tuples
+        """
+        return [(x.tag, x.text) for x in self.node]
+
+    def keys(self):
+        """
+        Returns list of keys
+        """
+        return [x[0] for x in self.items()]
+
+    def values(self):
+        """
+        Returns list of values
+        """
+        return [x[1] for x in self.items()]
+
+    def as_dict(self):
+        """
+        Returns a dictionary representation of the asset node.
+        """
+        return {x[0]: x[1] for x in self.items()}
+
+
+
+class AssetNode(DictNode):
+    """
+    Interface for target asset node.
+    """
+
+    def __init__(self, node: ET.Element):
+        prefix: str = ".//"
+        suffix: str = ""
+        immutable: bool = False
+        super().__init__(node, prefix, suffix, immutable)
+
+class StigInfoNode(DictNode):
+    """
+    Stig Info node providing a dict-like interface.
+
+    Args:
+        node (ET.Element): STIG_INFO node
+    Attributes:
+        immutable (bool): Whether dict interface is immutable
+        prefix (str): Element lookup prefix
+        suffix (str): Element lookup suffix
+    """
+
+    def __init__(self, node: ET.Element):
+        # self.node: ET.Element = node
+        immutable: bool = True
+        prefix: str = './/SI_DATA/SID_NAME[.="'
+        suffix: str = '"]/../SID_DATA'
+        super().__init__(node, prefix, suffix, immutable)
+
+    def items(self) -> List[str]:
+        key = ".//SID_NAME"
+        value = ".//SID_DATA"
+        result = []
+        for sid in self.node:
+            x = sid.find(key).text
+            try:
+                y = sid.find(value).text
+            except AttributeError:
+                sid.makeelement(value, {}).text = ""
+                y = ""
+            result.append((x, y))
+        return result
+
+
+class VulnNode(DictNode):
+    """
+    Vulnerability node providing properties for mutable elements, and a
+    dict-like interface to the STIG data.
+
+    Args:
+        node (ET.Element): VULN node
+
+    Attributes:
+        immutable (bool): Whether dict interface is immutable
+        prefix (str): Element lookup prefix
+        suffix (str): Element lookup suffix
+
+    Properties:
+        vuln_num (str): V-ID (immutable)
+        status (str): Status [Not_Reviewed, NotAFinding, Not_Applicable, Open]
+        finding_details (str): Finding details field
+        comments (str): Comments field
+        severity_override (str): Severity override
+        severity_justification (str): Severity justification
+    """
+
+    def __init__(self, node: ET.Element):
+        # self.node: ET.Element = node
+        immutable: bool = True
+        prefix: str = './/STIG_DATA/VULN_ATTRIBUTE[.="'
+        suffix: str = '"]/../ATTRIBUTE_DATA'
+        super().__init__(node, prefix, suffix, immutable)
+
+    @property
+    def vuln_num(self) -> str:
+        return self.node.find(f"{self.__prefix}Vuln_Num{self.__suffix}").text
+
+    @property
+    def status(self) -> str:
+        return self.node.find("STATUS").text
+
+    @status.setter
+    def status(self, value: str):
+        self.node.find("STATUS").text = value
+
+    @property
+    def finding_details(self) -> str:
+        return self.node.find("FINDING_DETAILS").text
+
+    @finding_details.setter
+    def finding_details(self, value: str):
+        self.node.find("FINDING_DETAILS").text = value
+
+    @property
+    def comments(self) -> str:
+        return self.node.find("COMMENTS").text
+
+    @comments.setter
+    def comments(self, value: str):
+        self.node.find("COMMENTS").text = value
+
+    @property
+    def severity_override(self) -> str:
+        return self.node.find("SEVERITY_OVERRIDE").text
+
+    @severity_override.setter
+    def severity_override(self, value: str):
+        self.node.find("SEVERITY_OVERRIDE").text = value
+
+    @property
+    def severity_justification(self) -> str:
+        return self.node.find("SEVERITY_JUSTIFICATION").text
+
+    @severity_justification.setter
+    def severity_justification(self, value: str):
+        self.node.find("SEVERITY_JUSTIFICATION").text = value
+
+    def items(self) -> List[str]:
+        key = ".//VULN_ATTRIBUTE"
+        value = ".//ATTRIBUTE_DATA"
+        return [
+            (x.find(key).text, x.find(value).text) for x in self.node.iter("STIG_DATA")
+        ]
+
+
+class StigNode:
+    """
+    Node representing the actual STIG
+    """
+
+    def __init__(self, stig: ET.Element):
+        self.node: ET.Element = stig
+        self.info: StigInfoNode = StigInfoNode(self.node.find(".//STIG_INFO"))
+        self.vuln_nodes: Dict[str, VulnNode] = {
+            x.find(
+                './/STIG_DATA/VULN_ATTRIBUTE[.="Vuln_Num"]/../ATTRIBUTE_DATA'
+            ).text: VulnNode(x)
+            for x in self.node.findall("VULN")
+        }
+
+    @property
+    def stigid(self) -> str:
+        return self.info["stigid"]
+
+    @property
+    def version(self) -> str:
+        return self.info["version"]
+
+    @property
+    def releaseinfo(self) -> str:
+        return self.info["releaseinfo"]
+
+
+class Checklist:
+    """
+    Interface to parse and build DISA checklist files.
+
+    Attributes:
+        file (Path): Path to checklist file
+        tree (ET.ElementTree): ElementTree object
+        root (ET.Element): Root element
+        asset (ET.Element): Asset element
+        stigs (List[ET.Element]): List of stig elements in file
+    """
+
+    def __init__(self, file: str, autoload: bool = True):
+        self.tree: ET.ElementTree = None
+        self.root: ET.Element = None
+        self.asset: ET.Element = None
+        self.stigs: List[ET.Element] = []
+        self.file: Path = Path(file)
+        if autoload:
+            self.load()
+
+    def as_dict(self) -> dict:
+        """
+        Return a summary of checklist data as a dictionary.
+
+        Returns:
+            dict: Checklist data
+        """
+        return {
+            "asset": self.asset.as_dict(),
+            "stigs": [
+                {
+                    "info": stig.info.as_dict(),
+                    "vulnerabilities": {
+                        v: stig.vuln_nodes[v].status for v in stig.vuln_nodes
+                    },
+                }
+                for stig in self.stigs
+                ],
+        }
+
+    def load(self):
+        """
+        Load checklist XML data from file.
+        """
+        self.parse()
+        self.load_asset()
+        self.load_stigs()
+
+    def parse(self):
+        """
+        Parse XML.
+        """
+        self.tree = ET.parse(self.file)
+        self.root = self.tree.getroot()
+
+    def load_asset(self):
+        """
+        Load asset data.
+        """
+        self.asset = AssetNode(self.root.find(".//ASSET"))
+
+    def load_stigs(self):
+        """
+        Load stig data.
+        """
+        self.stigs = [StigNode(x) for x in self.root.findall(".//STIGS/iSTIG")]
+
+    def save(self, output_file: Path, force: bool = False):
+        """
+        Save checklist XML to specified file.
+
+        Args:
+            output_file (Path): Path to output file
+        """
+        if not output_file.parent.is_dir():
+            output_file.parent.mkdir(parents=True)
+        if output_file.exists():
+            if not force:
+                raise FileExistsError(f"{output_file} already exists. Pass 'force=True' to overwrite.")
+            else:
+                output_file.unlink()
+        self.tree.write(output_file)
---
+++ src/pigsty/resources/openscap.py
@@ -0,0 +1,249 @@
+"""
+Placeholder
+"""
+
+import re
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, Set
+from xml.etree import ElementTree as ET
+
+NS: dict = {
+    "XMLSchema": "http://oval.mitre.org/XMLSchema/oval-results-5",
+    "xccdf": "http://checklists.nist.gov/xccdf/1.2",
+    "arf": "http://scap.nist.gov/schema/asset-reporting-format/1.1",
+    "oval-definitions": "http://oval.mitre.org/XMLSchema/oval-definitions-5",
+    "scap": "http://scap.nist.gov/schema/scap/source/1.2",
+    "oval-characteristics": "http://oval.mitre.org/XMLSchema/oval-system-characteristics-5",
+    "oval-object": "http://oval.mitre.org/XMLSchema/oval-definitions-5#independent",
+    "cpe-dict": "http://cpe.mitre.org/dictionary/2.0",
+    "ds": "http://scap.nist.gov/schema/scap/source/1.2",
+    "cpe-lang": "http://cpe.mitre.org/language/2.0",
+    "cdf": "http://checklists.nist.gov/xccdf/1.2",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "dc": "http://purl.org/dc/elements/1.1/",
+}
+
+INVALID_IPV4: set = {
+    "127.0.0.1",
+    "192.168.122.1",
+}
+
+INVALID_MAC: set = {
+    "00:00:00:00:00:00",
+}
+
+INVALID_HOSTNAME: set = {
+    "localhost",
+}
+
+INVALID_IPV6: set = {
+    "::1",
+}
+
+
+class OpenSCAPRuleResult:
+    """
+    Interface to parse and retrieve results from an OpenSCAP XCCDF result file in STIG Viewer format.
+
+    Args:
+        result_node (ET.Element): Root element
+
+    Parameters:
+        rule_id (str): Full rule ID
+        vid (str): V-ID in format 'V-[0-9]+'
+        time (datetime): Scan time
+        result (str): Result of check
+        severity (str): Severity rating
+        weight (str): Numeric weight of check
+        as_dict (dict): Dictionary representation of the OpenSCAPRuleResult object
+    """
+
+    def __init__(self, result_node: ET.Element):
+        super().__init__()
+        self.rule_id: str
+        self.vid: str
+        self.time: datetime
+        self.result: str
+        self.__datetime_format: str = "%Y-%m-%dT%H:%M:%S%z"
+        self.node = result_node
+
+    @property
+    def rule_id(self) -> str:
+        return self.node.get("idref")
+
+    @property
+    def vid(self) -> str:
+        return re.search(r"V-[0-9]+", self.rule_id).group(0)
+
+    @property
+    def time(self) -> datetime:
+        return datetime.strptime(self.node.get("time"), self.__datetime_format)
+
+    @property
+    def severity(self) -> str:
+        return self.node.get("severity")
+
+    @property
+    def weight(self) -> str:
+        return self.node.get("weight")
+
+    @property
+    def result(self) -> str:
+        return self.node.get("result")
+
+    def as_dict(self) -> dict:
+        """
+        Returns a dictionary representation of the OpenSCAPRuleResult object
+        """
+        ident = self.node.find("xccdf:ident", NS)
+        check = self.node.find("xccdf:check", NS)
+        if check:
+            check_export: list = self.node.findall("xccdf:check-export")
+            check_content_ref = self.node.find("xccdf:check-content-ref", NS)
+            check_data: dict = (
+                {
+                    "system": check.get("system"),
+                    "check_content_ref": {
+                        "name": check_content_ref.get("name"),
+                        "href": check_content_ref.get("href"),
+                    }
+                    if check_content_ref
+                    else None,
+                    "check_export": [
+                        {
+                            "export_name": x.get("export_name"),
+                            "href": x.get("href"),
+                        }
+                        for x in check_export
+                    ],
+                }
+                if check
+                else None,
+            )
+
+        return {
+            "rule_id": self.rule_id,
+            "vid": self.vid,
+            "time": self.time,
+            "severity": self.severity,
+            "weight": self.weight,
+            "result": self.result,
+            "ident": {"system": ident.get("system"), "text": ident.text},
+            "check": check_data if check else None,
+        }
+
+
+class OpenSCAPXccdfResult:
+    """
+    Interface to parse and retrieve results from an OpenSCAP XCCDF result file in STIG Viewer format.
+
+    Args:
+        file (str): Path to XCCDF file
+        autoload (bool): Whether to load the data immediately
+
+    Attributes:
+        file (Path): Path to XCCDF file
+        tree (ET.ElementTree): ElementTree object
+        root (ET.Element): Root element
+        rule_results (Dict[str, OpenSCAPRuleResult]): Dictionary of OpenSCAPRuleResult objects
+
+    Properties:
+        target (str): Target name
+        identity (str): Identity of scan operator
+        target_addresses (Set[str]): Values of target-address fields
+        fqdn (Set[str]): Values FQDN target-facts
+    """
+
+    def __init__(self, file: str, autoload: bool = True):
+        self.tree: ET.ElementTree = None
+        self.root: ET.Element = None
+        self.rule_results: Dict[str, OpenSCAPRuleResult] = {}
+        self.file: Path = Path(file)
+        if autoload:
+            self.load()
+
+    def load(self):
+        """
+        Load checklist XML data from file.
+        """
+        self._parse()
+        self._load_rule_results()
+
+    def _parse(self):
+        """
+        Parse XML.
+        """
+        self.tree = ET.parse(self.file)
+        self.root = self.tree.getroot()
+
+    def _load_rule_results(self):
+        """
+        Load the rule results as OpenSCAPRuleResult objects in the rule_results dictionary.
+        """
+        vid_regex = r"V-[0-9]+"
+        rule_results = self.root.findall(".//xccdf:rule-result", NS)
+        for result in rule_results:
+            rule_id = result.get("idref")
+            vid = re.search(vid_regex, rule_id).group(0)
+            self.rule_results.update({vid: OpenSCAPRuleResult(result)})
+
+    @property
+    def target(self) -> str:
+        return self.root.find(".//xccdf:target", NS).text
+
+    @property
+    def identity(self) -> str:
+        return self.root.find(".//xccdf:identity", NS).text
+
+    @property
+    def target_addresses(self) -> Set[str]:
+        super = INVALID_IPV4.union(INVALID_IPV6, INVALID_HOSTNAME, INVALID_MAC)
+        return {
+            x.text
+            for x in self.root.findall(".//xccdf:target-address", NS)
+            if x.text not in super
+        }
+
+    @property
+    def ipv4(self) -> Set[str]:
+        query = './/xccdf:target-facts/xccdf:fact[@name="urn:xccdf:fact:asset:identifier:ipv4"]'
+        return {
+            x.text for x in self.root.findall(query, NS) if x.text not in INVALID_IPV4
+        }
+
+    @property
+    def ipv6(self) -> Set[str]:
+        query = './/xccdf:target-facts/xccdffact[@name="urn:xccdf:fact:asset:identifier:ipv6"]'
+        return {
+            x.text for x in self.root.findall(query, NS) if x.text not in INVALID_IPV6
+        }
+
+    @property
+    def mac(self) -> Set[str]:
+        query = './/xccdf:target-facts/xccdf:fact[@name="urn:xccdf:fact:asset:identifier:mac"]'
+        return {
+            x.text for x in self.root.findall(query, NS) if x.text not in INVALID_MAC
+        }
+
+    @property
+    def hostname(self) -> Set[str]:
+        query = './/xccdf:target-facts/xccdf:fact[@name="urn:xccdf:fact:asset:identifier:host_name"]'
+        return {
+            x.text
+            for x in self.root.findall(query, NS)
+            if x.text not in INVALID_HOSTNAME
+        }
+
+    @property
+    def fqdn(self) -> Set[str]:
+        query = './/xccdf:target-facts/xccdf:fact[@name="urn:xccdf:fact:asset:identifier:fqdn"]'
+        return {x.text for x in self.root.findall(query, NS)}
+
+    @property
+    def platform(self) -> Set[str]:
+        return {x.get("idref") for x in self.root.findall(".//xccdf:platform", NS)}
+
+    @property
+    def cpe(self) -> Set[str]:
+        return {x for x in self.platform if x.startswith("cpe:/o:")}
